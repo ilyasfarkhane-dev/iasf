41ec44dd547101fb5288c23073e8b32bf3060ddc
Ilyas Farkhane <125192731+ilyasfarkhane-dev@users.noreply.github.com>
2024-12-26 12:57:06 +0100

    status fix

--- app/components/StatusBadge.tsx
+++ app/components/StatusBadge.tsx
@@ -1,9 +1,16 @@
-const StatusBadge = ({ status }) => {
+type StatusType = "Disponible" | "Maintenance" | "reservé";
+
+interface StatusBadgeProps {
+  status: StatusType;
+}
+
+const StatusBadge: React.FC<StatusBadgeProps> = ({ status }) => {
   const statusClasses = {
     Disponible: "bg-green-100 text-green-800",
     Maintenance: "bg-yellow-100 text-yellow-800",
     reservé: "bg-red-100 text-red-800",
   };
+
   return (
     <span
       className={`inline-block rounded-full px-3 py-1 text-xs font-medium ${statusClasses[status]}`}
